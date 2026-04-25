34ba1487bbbb90cb01d78d953845c721faec9206
MineKing <mineking9534@gmail.com>
2026-04-25 09:22:29 +0200

    feat: add switch to decide whether to show turn numbers

--- discord/src/main/kotlin/de/mineking/hexo/discord/EmojiManager.kt
+++ discord/src/main/kotlin/de/mineking/hexo/discord/EmojiManager.kt
@@ -7,25 +7,28 @@ import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.entities.Icon
 import net.dv8tion.jda.api.entities.emoji.Emoji
 
-enum class EmojiType(val path: String) {
+enum class CustomEmoji(val path: String) {
     PlayerX("hexo_player_x"),
     PlayerO("hexo_player_o"),
+
+    SwitchOn("switch_on"),
+    SwitchOff("switch_off"),
 }
 
-val EmojiType.discordName get() = name.lowercase()
+val CustomEmoji.discordName get() = name.lowercase()
 
 class EmojiManager(val jda: JDA) {
-    private val emojis = mutableMapOf<EmojiType, Emoji>()
+    private val emojis = mutableMapOf<CustomEmoji, Emoji>()
 
     init {
         runBlocking {
             val data = jda.retrieveApplicationEmojis().await()
-            data.filter { emoji -> EmojiType.entries.none { it.discordName == emoji.name } }.forEach {
+            data.filter { emoji -> CustomEmoji.entries.none { it.discordName == emoji.name } }.forEach {
                 logger.info { "Deleting unused emoji: ${it.name}" }
                 it.delete().queue()
             }
 
-            emojis += EmojiType.entries.associateWith { type ->
+            emojis += CustomEmoji.entries.associateWith { type ->
                 val emoji = data.find { type.discordName == it.name }
 
                 val currentImage = emoji?.image?.downloadAsIcon()?.await()
@@ -48,9 +51,9 @@ class EmojiManager(val jda: JDA) {
         }
     }
 
-    operator fun get(type: EmojiType) = emojis[type]!!
+    operator fun get(type: CustomEmoji) = emojis[type]!!
 }
 
-private fun loadIcon(type: EmojiType) =
+private fun loadIcon(type: CustomEmoji) =
     EmojiManager::class.java.classLoader.getResourceAsStream("emoji/${type.path}.png")?.use { Icon.from(it) }
         ?: error("Could not load icon for emoji type: $type")
--- history/src/main/kotlin/de/mineking/hexo/history/Match.kt
+++ history/src/main/kotlin/de/mineking/hexo/history/Match.kt
@@ -102,13 +102,16 @@ data class Match(
     )
     val playerMappings = playerIdMappings.entries.associate { (id, player) -> player to players.first { it.playerId == id } }
 
-    fun asBoard(lastMove: Int = moveCount) = Board().apply {
+    fun asBoard(lastMove: Int = moveCount, showTurnNumber: Boolean = false) = Board().apply {
         repeat(lastMove.coerceIn(0, moveCount)) {
             val move = moves[it]
 
             val cell = this[move.q, move.r]
             cell.owner = playerIdMappings[move.playerId]
-            cell.turn = (it + 1) / 2
+
+            if (showTurnNumber) {
+                cell.turn = (it + 1) / 2
+            }
 
             if (it == lastMove - 1) cell.focussed = true
         }
